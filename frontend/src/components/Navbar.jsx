4aaa0ff71807e003c3527abc0c489f7c179b070d
Abhin <iamcaptaincrgmail.com>
2026-08-07 13:15:38 +0530

    second commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 function Navbar() {
   return (
     <Box bg="teal.500" color="white" p={5} boxShadow="lg">
-      <Heading size="lg">Product Management System</Heading>
+      <Heading size="lg">ABC Retail Products</Heading>
       <Text fontSize="sm" mt={1}>
         
       </Text>
